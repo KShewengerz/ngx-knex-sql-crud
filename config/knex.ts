d0a9d9bbb170d1df4008a7c12dc20f0000e6564c
Kristy Mae Almuete <kristyalmuete@gmail.com>
2019-02-08 22:46:21 +0800

    #1 Setup KnexJS Configuration and Npm Migration Commands

---
+++ config/knex.ts
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+
+const knex       = require("knex");
+const knexConfig = require("../knexfile");
+
+const env     = process.env.NODE_ENV;
+const config  = knexConfig[env];
+
+export default knex(config);
---
+++ knexfile.ts
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+
+module.exports = {
+  
+  development: {
+    client: process.env.DEV_DB_CLIENT,
+    connection  : {
+      database  : process.env.DEV_DB_NAME,
+      user      : process.env.DEV_DB_USERNAME,
+      password  : process.env.DEV_DB_PASSWORD
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      loadExtensions: ['.ts']
+    }
+  }
+  
+};
