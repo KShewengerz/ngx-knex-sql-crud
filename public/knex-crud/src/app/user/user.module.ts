8c220a597932956891c8e6dec563453f917eb2b5
Kristy Mae Almuete <kristyalmuete@gmail.com>
2019-02-10 14:28:31 +0800

    #6 Setup UserListComponent and #7 UserFormComponent and Update angular version to 7. Change font from karla to oxygen

--- public/knex-crud/src/app/user/user.module.ts
+++ public/knex-crud/src/app/user/user.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { UserComponent } from '@app/user/user.component';
+import { UserComponent } from '@app/user/containers/user.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 
 @NgModule({
-  declarations: [ UserComponent ],
+  declarations: [
+    UserComponent,
+    UserFormComponent,
+    UserListComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
